e2aed6626b5146e225dad5a55d97baecf4ef536d
Vinod Kumar <vinodkumar@Vinods-MacBook-Air.local>
2025-03-10 11:51:48 +0530

    test code

---
+++ test.py
@@ -0,0 +1,17 @@
+def sum(a,b):
+    print("Summation is: ", a+b)
+
+def mult(a,b):
+    print("Multipication is: ", a*b)
+
+def square(a):
+    print("square is: ", a*a)
+
+
+
+if __name__ == '__main__':
+    a = 4
+    b= 8
+    sum(a,b)
+    mult(a,b)
+    square(a)
